ab7e805f73c1216cf29570e1b2ff909627a45210
Oleg Dubinkin <odubinkin@gmail.com>
2023-07-24 14:00:00 +0700

    feat: fix address

--- src/iframe.js
+++ src/iframe.js
@@ -67,7 +67,7 @@ const RequestLink = () => {
   const NftProtectContract = useMemo(() => {
     if (!parameters) return
     if (!arbitrableSigner) return
-    const address = supportedChains.find(chain => chain.id.toString() === parameters.arbitrableChainID)
+    const address = supportedChains.find(chain => chain.id.toString() === parameters.arbitrableChainID).arbitrableProxyAddress
     if (!address) {
       console.error(`nftprotect contract not found for chainId ${parameters.arbitrableChainID}`)
       setErrored({
