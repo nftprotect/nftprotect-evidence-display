2e762d0a48f7ffdb2fb146a357297e1051ad197e
Oleg Dubinkin <odubinkin@gmail.com>
2024-02-26 22:44:20 +0700

    feat: update for new contract

--- src/config.js
+++ src/config.js
@@ -1,17 +1,10 @@
 export const supportedChains = [
     {
-        id: 5,
-        name: "Goerli",
-        symbol: "ETH",
-        nodeUrl: "https://goerli.infura.io/v3/c060090dffdd401d8e4e0bc4c6d5e5f0",
-        nftpContractAddress: '0x5aDf8b955011a09726CbAB8d43622ac1B2fB344F',
-        arbitrableProxyAddress: '0x78ac5F189FC6DAB261437a7B95D11cAcf0234FFe'
-    }, {
         id: 11155111,
         name: "Sepolia",
         symbol: "ETH",
         nodeUrl: "https://sepolia.infura.io/v3/c060090dffdd401d8e4e0bc4c6d5e5f0",
-        nftpContractAddress: '0xB1A382a0946528cAc1Bde3f419B69809CC347613',
+        nftpContractAddress: '0xe7807e973f42aea32da71c15a6441760f675d9cc',
         arbitrableProxyAddress: '0x1780601E6465F32233643F3af54abC3D8dF161BE'
     },
 ]
